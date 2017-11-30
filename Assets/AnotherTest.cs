9d652560db74870725c16210235978f11a8e8553
GITBSB <bsbenny.bh@gmail.com>
2017-11-30 20:15:06 +0100

    added synths

--- Assets/AnotherTest.cs
+++ Assets/AnotherTest.cs
@@ -5,8 +5,40 @@ using System;
 
 public class AnotherTest : MonoBehaviour {
 
+	int n_letters = 5;
+	int n_used_letters = 2;
+
+
 	void OnMouseEnter() {
 
+		int max_used_letters = 64 + n_used_letters;
+
+		ErzeugeAlleWorte ("", n_letters, max_used_letters);
+	}
+		
+
+	private static void ErzeugeAlleWorte (String strBegin, int length, int max_letters)
+	{
+		if (length <= 0) {
+			Debug.Log(strBegin);
+			return;
+		}
+
+		for(int i = 65; i <= max_letters; i++) {
+			ErzeugeAlleWorte (strBegin + Convert.ToChar(i), length - 1, max_letters);
+		}
 	}
 }
+
+
+
+
+
+
+
+
+
+
 // SCInterface.Instance.SetNodeValue (100, "delay", 0.6);
+
+
